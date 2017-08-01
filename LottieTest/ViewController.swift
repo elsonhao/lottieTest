d02d67de7e411c60f93848141224ce6491f1aae0
elson Huang <elson.huang@jamzoo.com.tw>
2017-08-01 12:28:32 +0800

    lottie 動畫

--- LottieTest/ViewController.swift
+++ LottieTest/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let myAnimationView = LOTAnimationView(name: "timer_(3_second_loop)"){
+            myAnimationView.frame = CGRect(x: 150, y: 50, width: 100, height: 100)
+           // myAnimationView.center = self.view.center
+            myAnimationView.contentMode = .scaleAspectFill
+            myAnimationView.loopAnimation = true
+            view.addSubview(myAnimationView)
+            myAnimationView.play()
+        }
+        if let myAnimationView = LOTAnimationView(name: "pink_drink_machine"){
+            myAnimationView.frame = CGRect(x: 150, y: 250, width: 100, height: 100)
+            // myAnimationView.center = self.view.center
+            myAnimationView.contentMode = .scaleAspectFill
+            myAnimationView.loopAnimation = true
+            view.addSubview(myAnimationView)
+            myAnimationView.play()
+        }
+        if let myAnimationView = LOTAnimationView(name: "crown"){
+            myAnimationView.frame = CGRect(x: 150, y: 450, width: 300, height: 300)
+            // myAnimationView.center = self.view.center
+            myAnimationView.contentMode = .scaleAspectFill
+            myAnimationView.loopAnimation = true
+            
+            
+            let minimizeTransform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            myAnimationView.transform = minimizeTransform
+            
+            //apply uiview animation
+            UIView.animate(withDuration: 3.0, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+                myAnimationView.transform = CGAffineTransform.identity
+            }, completion: nil)
+            view.addSubview(myAnimationView)
+            myAnimationView.play()
+        }
+        
+
     }
 
     override func didReceiveMemoryWarning() {
